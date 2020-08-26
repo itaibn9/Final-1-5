4a19f531dd5ad4493021ac7c898f68d678a9760a
itaibn9 <itaibn9@gmail.com>
2020-08-26 16:11:27 +0300

    eslint modify

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import '.././styling/App.css';
-import Ticket from './Ticket';
+import React, { useEffect, useState } from 'react';
+import '../styling/App.css';
 import axios from 'axios';
+import Ticket from './Ticket';
 import Search from './Search';
+
 function App() {
   const [tickets, setTickets] = useState([]);
   const [counter, setCounter] = useState(0);
@@ -10,45 +11,59 @@ function App() {
   const [callrestore, setCallrestore] = useState(0);
   const hideTheTicket = () => {
     setCounter(counter + 1);
-  }
+  };
   const filterBySearch = async (title) => {
-      try{
-        const data = await axios.get(`/api/tickets?searchText=${title}`);
-        setTickets(data.data);
-        setNumOfTickets(data.data.length)
-      } catch(error) {
-         console.log(error);
-      }
-  }
-  useEffect(()=>{
+    try {
+      const data = await axios.get(`/api/tickets?searchText=${title}`);
+      setTickets(data.data);
+      setNumOfTickets(data.data.length);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
     (async () => {
-      try{
-        const { data } = await axios.get(`/api/tickets`);
+      try {
+        const { data } = await axios.get('/api/tickets');
         setNumOfTickets(data.length);
         setTickets(data);
-      } catch(error) {
+      } catch (error) {
         console.log(error);
       }
-    })()
-  },[])
+    })();
+  }, []);
   const restoreHiddenTickets = () => {
     setCounter(0);
     setCallrestore(callrestore + 1);
-      }
+  };
   return (
     <main>
       <header>
-      <h1>My Ticket Manager</h1>
+        <h1>My Ticket Manager</h1>
       </header>
       <div id="searchInput">
-  <div> {numOfTickets} results {counter > 0 ? (<span>(Hidden tickets: <span id="hideTicketsCounter">
-        {counter}</span><b> - </b>
-        <button id="restoreHideTickets" onClick={restoreHiddenTickets}>restore</button>)</span>): ''} </div>
-        <Search onchange={filterBySearch}/>
+        <div>
+          {' '}
+          {numOfTickets}
+          {' '}
+          results
+          {' '}
+          {counter > 0 ? (
+            <span>
+              (Hidden tickets:
+              <span id="hideTicketsCounter">
+                {counter}
+              </span>
+              <b> - </b>
+              <button id="restoreHideTickets" onClick={restoreHiddenTickets}>restore</button>
+              )
+            </span>
+          ) : ''}
+          {' '}
+        </div>
+        <Search onchange={filterBySearch} />
       </div>
-      {tickets.map(i => 
-          <Ticket ticket={i} hideOnClick={hideTheTicket} callRestore={callrestore}/> 
-        )}
+      {tickets.map((i) => <Ticket ticket={i} hideOnClick={hideTheTicket} callRestore={callrestore} />)}
     </main>
   );
 }
--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,5 +1,5 @@
-import React,{ useState, useEffect } from 'react';
-import '.././styling/Ticket.css';
+import React, { useState, useEffect } from 'react';
+import '../styling/Ticket.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -24,44 +24,62 @@ const useStyles = makeStyles({
   },
 });
 
-
-function Tickets({ticket, hideOnClick, callRestore}) {
-  const [ classTicket, setClassTicket] = useState('ticket');
-    const classes = useStyles();
-    function changeTheDate(theDate) {
-      const current = new Date(theDate)
-      const timestring = (current.getDay() + 1) + '/' + (current.getMonth() + 1) + '/' + current.getFullYear() + ' ' + current.toLocaleTimeString();
-      return timestring;
-    }
-
-    useEffect(() => {
-      setClassTicket('ticket');
-    }, [callRestore])
-
-    return (
-<div id={ticket.id} key={ticket.id} className={classTicket}>
-<Card className={classes.root}>
-  <button className="hideTicketButton" onClick={() => {
-    setClassTicket('hiddenTicket');
-    hideOnClick()
-  }}>Hide</button>
-<CardContent>
-  <Typography variant="h5" component="h2">
-  {ticket.title}
-  </Typography>
-  <Typography variant="body2" component="p">
-  {ticket.content}
-  </Typography>
-</CardContent>
-<CardActions className="lowerBar">
-<span>by {ticket.userEmail} | {changeTheDate(ticket.creationTime)} </span>    
-      <span>{ticket.labels ? (ticket.labels).map(element => {
-        return  <Button style={{textTransform:"none"}} className="label" size="small">{element}</Button> }) : null}
-        </span>
-</CardActions>
-</Card>
-  </div>
-    )
+function Tickets({ ticket, hideOnClick, callRestore }) {
+  const [classTicket, setClassTicket] = useState('ticket');
+  const classes = useStyles();
+  function changeTheDate(theDate) {
+    const current = new Date(theDate);
+    const timestring = `${current.getDay() + 1}/${current.getMonth() + 1}/${current.getFullYear()}
+     ${current.toLocaleTimeString()}`;
+    return timestring;
+  }
+  useEffect(() => {
+    setClassTicket('ticket');
+  }, [callRestore]);
+  return (
+    <div id={ticket.id} key={ticket.id} className={classTicket}>
+      <Card className={classes.root}>
+        <button
+          type="submit"
+          className="hideTicketButton"
+          onClick={() => {
+            setClassTicket('hiddenTicket');
+            hideOnClick();
+          }}
+        >
+          Hide
+        </button>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {ticket.title}
+          </Typography>
+          <Typography variant="body2" component="p">
+            {ticket.content}
+          </Typography>
+        </CardContent>
+        <CardActions className="lowerBar">
+          <span>
+            by
+            {ticket.userEmail}
+            {' '}
+            |
+            {changeTheDate(ticket.creationTime)}
+          </span>
+          <span>
+            {ticket.labels ? (ticket.labels).map((element) => (
+              <Button
+                style={{ textTransform: 'none' }}
+                className="label"
+                size="small"
+              >
+                {element}
+              </Button>
+            )) : null}
+          </span>
+        </CardActions>
+      </Card>
+    </div>
+  );
 }
 
 export default Tickets;
